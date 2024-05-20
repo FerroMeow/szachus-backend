397b5d57490ed3a6a9e3017e9a48c77bd2a6bc3f
Konrad Balcerzak <kobdi4@gmail.com>
2024-05-20 20:15:15 +0200

    Add init stuff

---
+++ migrations/20240401164241_init.sql
@@ -0,0 +1,5 @@
+-- Add migration script here
+CREATE TABLE user {
+  id INT GENERATED ALWAYS AS IDENTITY,
+  
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use axum::{routing::get, Router};
+use std::{env, fs};
 
 #[tokio::main]
-async fn main() {
-    // build our application with a single route
+async fn main() -> anyhow::Result<()> {
+    let password_file = env::var("PASSWORD_FILE")?;
+    let password = String::from_utf8(fs::read(password_file)?)?;
+    let connection_string = format!("postgres://postgres:{}@localhost/szachus", &password);
+    let db_pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&connection_string);
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
